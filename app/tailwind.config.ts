97b0976d95f48fb2e85e17193fabe0c8fd607343
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-12-10 11:31:52 -0500

    app: fix build

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -6,6 +6,13 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
   plugins: [],
 } satisfies Config
