68540dad4e701689ff9d5632051f89160e239d04
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-08-06 15:55:27 -0400

    Temporarily hardcode Ponder URL

--- server/src/proposals.ts
+++ server/src/proposals.ts
@@ -1,9 +1,11 @@
 // TODO: This is a copy of the client/src/hooks/useProposal.ts file so we should move that to `shared`
 import type { EnhancedProposalWithVotes } from 'indexer/types'
 
+const ponderUrl = 'https://ens-governor.ponder-dev.com/'
+
 export async function getProposal(id: string) {
   const path = `/proposals/${id}`
-  const url = new URL(path, process.env.PONDER_URL).toString()
+  const url = new URL(path, ponderUrl).toString()
 
   const response = await fetch(url)
   if (!response.ok) {
